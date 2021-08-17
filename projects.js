be555025a4d3eb22182a07c37388343bf7a8447d
macluiggy <ferrinluiggy@gmail.com>
2021-08-17 11:42:24 -0500

    updated

---
+++ projects.js
@@ -0,0 +1,11 @@
+export default 
+[
+	{
+		"title": "Simple Markdown Previewer",
+		"logos": ["javascript", "sass", "react", "redux"]
+	},
+	{
+		"title": "Simple Markdown Previewer 222",
+		"logos": ["javascript", "sass", "react"]
+	},
+]
